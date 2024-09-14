bad1440f253a6d9693b46389804b27e58e802923
Hussein Najah <husseinnajah123@gmail.com>
2024-09-14 14:13:37 +0300

    block

--- frontend/src/components/CategoryWiseProductDisplay.js
+++ frontend/src/components/CategoryWiseProductDisplay.js
@@ -18,16 +18,17 @@ const CategoryWiseProductDisplay = ({ category, heading }) => {
     fetchUserAddToCart();
   };
 
-  const fetchData = async () => {
-    setLoading(true);
-    const categoryProduct = await fetchCategoryWiseProduct(category);
-    setLoading(false);
-
-    setData(categoryProduct?.data);
-  };
   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const categoryProduct = await fetchCategoryWiseProduct(category);
+      setLoading(false);
+
+      setData(categoryProduct?.data);
+    };
+
     fetchData();
-  }, []);
+  }, [category]);
 
   return (
     <div className="container relative px-4 mx-auto my-6">
